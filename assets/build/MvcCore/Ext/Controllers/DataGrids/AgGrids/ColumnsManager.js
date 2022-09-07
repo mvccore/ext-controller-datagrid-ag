617f2a08d7b43354f39c1ac5dc57fc744b38e9b1
Tom Flídr <tomflidr@gmail.com>
2022-09-07 20:54:50 +0200

    regenerated client scripts

--- assets/build/MvcCore/Ext/Controllers/DataGrids/AgGrids/ColumnsManager.js
+++ assets/build/MvcCore/Ext/Controllers/DataGrids/AgGrids/ColumnsManager.js
@@ -56,6 +56,7 @@ var MvcCore;
                                 editable: false,
                                 flex: 1,
                                 headerComponent: AgGrids.ColumnsManagers.SortHeader,
+                                //tooltipComponent: AgGrids.ToolTip TODO
                             };
                             this.sortHeaderDefaults = {
                                 renderDirection: true,

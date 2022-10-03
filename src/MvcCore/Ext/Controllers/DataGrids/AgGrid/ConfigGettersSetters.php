c1192272d37ed52d402d97fa15d1e26218e25c92
Tom Flídr <tomflidr@gmail.com>
2022-10-03 18:21:42 +0200

    configuration methods refactoring

--- src/MvcCore/Ext/Controllers/DataGrids/AgGrid/ConfigGettersSetters.php
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrid/ConfigGettersSetters.php
@@ -21,35 +21,7 @@ use \MvcCore\Ext\Controllers\IDataGrid;
 trait ConfigGettersSetters {
 	
 	/**
-	 * @param  string $jsClassFullName 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetJsClassFullName ($jsClassFullName) {
-		$this->jsClassFullName = $jsClassFullName;
-		return $this;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function GetJsClassFullName () {
-		return $this->jsClassFullName;
-	}
-	
-	
-	/**
-	 * @inheritDocs
-	 * @param  \int[] $countScales
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetCountScales (array $countScales) {
-		parent::SetCountScales($countScales);
-		$this->countScalesCustomized = TRUE;
-		return $this;
-	}
-	
-
-	/**
+	 * 
 	 * @param  string $id 
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
 	 */
@@ -60,6 +32,7 @@ trait ConfigGettersSetters {
 	}
 
 	/**
+	 * 
 	 * @return string|NULL
 	 */
 	public function GetId () {
@@ -67,200 +40,7 @@ trait ConfigGettersSetters {
 	}
 	
 	/**
-	 * @param  bool $enabledColumnsOnly 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetEnabledColumnsOnly ($enabledColumnsOnly) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->enabledColumnsOnly = $enabledColumnsOnly;
-		return $this;
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function GetEnabledColumnsOnly () {
-		return $this->enabledColumnsOnly;
-	}
-	
-	
-	/**
-	 * @param  int $clientPageMode 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetClientPageMode ($clientPageMode) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->clientPageMode = $clientPageMode;
-		return $this;
-	}
-
-	/**
-	 * @return int|NULL
-	 */
-	public function GetClientPageMode () {
-		return $this->clientPageMode;
-	}
-
-	/**
-	 * @param  int $clientRequestBlockSize 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetClientRequestBlockSize ($clientRequestBlockSize) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->clientRequestBlockSize = $clientRequestBlockSize;
-		return $this;
-	}
-
-	/**
-	 * @return int|NULL
-	 */
-	public function GetClientRequestBlockSize () {
-		return $this->clientRequestBlockSize;
-	}
-	
-	/**
-	 * @param  int $clientRowBuffer 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetClientRowBuffer ($clientRowBuffer) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->clientRowBuffer = $clientRowBuffer;
-		return $this;
-	}
-
-	/**
-	 * @return int|NULL
-	 */
-	public function GetClientRowBuffer () {
-		return $this->clientRowBuffer;
-	}
-	
-	/**
-	 * @param  string $urlData 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetUrlData ($urlData) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->urlData = $urlData;
-		return $this;
-	}
-
-	/**
-	 * @return string|NULL
-	 */
-	public function GetUrlData () {
-		return $this->urlData;
-	}
-
-	/**
-	 * @param  \DateInterval $timeZoneOffset 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetTimeZoneOffset (\DateInterval $timeZoneOffset) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->timeZoneOffset = $timeZoneOffset;
-		return $this;
-	}
-
-	/**
-	 * @return \DateInterval|NULL
-	 */
-	public function GetTimeZoneOffset () {
-		if ($this->timeZoneOffset === NULL) {
-			$this->timeZoneOffset = new \DateInterval("PT0H0M0S");
-		}
-		return $this->timeZoneOffset;
-	}
-
-	/**
-	 * @param  int $dataRequestMethod 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetDataRequestMethod ($dataRequestMethod) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->dataRequestMethod = $dataRequestMethod;
-		return $this;
-	}
-
-	/**
-	 * @return int
-	 */
-	public function GetDataRequestMethod () {
-		return $this->dataRequestMethod;
-	}
-	
-	/**
-	 * @param  int $rowSelection 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetRowSelection ($rowSelection) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->rowSelection = $rowSelection;
-		return $this;
-	}
-
-	/**
-	 * @return int
-	 */
-	public function GetRowSelection () {
-		return $this->rowSelection;
-	}
-	
-	/**
-	 * @param  int|NULL $clientMaxRowsInCache 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetClientMaxRowsInCache ($clientMaxRowsInCache) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->clientMaxRowsInCache = $clientMaxRowsInCache;
-		return $this;
-	}
-
-	/**
 	 * 
-	 * @return int|NULL
-	 */
-	public function GetClientMaxRowsInCache () {
-		return $this->clientMaxRowsInCache;
-	}
-
-	/**
-	 * @param  bool $clientCache 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetClientCache ($clientCache) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->clientCache = $clientCache;
-		return $this;
-	}
-
-	/**
-	 * 
-	 * @return bool
-	 */
-	public function GetClientCache () {
-		return $this->clientCache;
-	}
-
-	/**
-	 * @param  array<string, string> $ajaxParamsNames 
-	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
-	 */
-	public function SetAjaxParamsNames (array $ajaxParamsNames) {
-		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
-		$this->ajaxParamsNames = $ajaxParamsNames;
-		return $this;
-	}
-
-	/**
-	 * @return array<string, string>
-	 */
-	public function GetAjaxParamsNames () {
-		return $this->ajaxParamsNames;
-	}
-	
-	/**
-	 * @inheritDocs
 	 * @param  \MvcCore\Ext\Controllers\DataGrids\AgGrids\Configs\Rendering $configRendering
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
 	 */
@@ -281,7 +61,7 @@ trait ConfigGettersSetters {
 	}
 
 	/**
-	 * @inheritDocs
+	 * 
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrids\Configs\Rendering
 	 */
 	#[\ReturnTypeWillChange]
@@ -291,9 +71,9 @@ trait ConfigGettersSetters {
 		}
 		return $this->configRendering;
 	}
-
+	
 	/**
-	 * @inheritDocs
+	 * 
 	 * @param  \MvcCore\Ext\Controllers\DataGrids\AgGrids\Configs\Locales $configRendering
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
 	 */
@@ -304,7 +84,7 @@ trait ConfigGettersSetters {
 	}
 
 	/**
-	 * @inheritDocs
+	 * 
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrids\Configs\Locales
 	 */
 	public function GetConfigLocales () {
@@ -313,9 +93,258 @@ trait ConfigGettersSetters {
 		}
 		return $this->configLocales;
 	}
+	
+	/**
+	 * 
+	 * @param  string $jsClassFullName 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetJsClassFullName ($jsClassFullName) {
+		$this->jsClassFullName = $jsClassFullName;
+		return $this;
+	}
 
 	/**
-	 * @inheritDocs
+	 * 
+	 * @return string
+	 */
+	public function GetJsClassFullName () {
+		return $this->jsClassFullName;
+	}
+	
+	/**
+	 * 
+	 * @param  bool $enabledColumnsOnly 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetEnabledColumnsOnly ($enabledColumnsOnly) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->enabledColumnsOnly = $enabledColumnsOnly;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return bool
+	 */
+	public function GetEnabledColumnsOnly () {
+		return $this->enabledColumnsOnly;
+	}
+	
+	/**
+	 * 
+	 * @param  int $rowSelection 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetRowSelection ($rowSelection) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->rowSelection = $rowSelection;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return int
+	 */
+	public function GetRowSelection () {
+		return $this->rowSelection;
+	}
+	
+	/**
+	 * 
+	 * @param  \DateInterval $timeZoneOffset 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetTimeZoneOffset (\DateInterval $timeZoneOffset) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->timeZoneOffset = $timeZoneOffset;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return \DateInterval|NULL
+	 */
+	public function GetTimeZoneOffset () {
+		if ($this->timeZoneOffset === NULL) {
+			$this->timeZoneOffset = new \DateInterval("PT0H0M0S");
+		}
+		return $this->timeZoneOffset;
+	}
+	
+	/**
+	 * 
+	 * @param  bool $clientCache 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetClientCache ($clientCache) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->clientCache = $clientCache;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return bool
+	 */
+	public function GetClientCache () {
+		return $this->clientCache;
+	}
+	
+	/**
+	 * 
+	 * @param  int $clientPageMode 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetClientPageMode ($clientPageMode) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->clientPageMode = $clientPageMode;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return int|NULL
+	 */
+	public function GetClientPageMode () {
+		return $this->clientPageMode;
+	}
+	
+	/**
+	 * 
+	 * @param  int $clientRequestBlockSize 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetClientRequestBlockSize ($clientRequestBlockSize) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->clientRequestBlockSize = $clientRequestBlockSize;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return int|NULL
+	 */
+	public function GetClientRequestBlockSize () {
+		return $this->clientRequestBlockSize;
+	}
+	
+	/**
+	 * 
+	 * @param  int $clientRowBuffer 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetClientRowBuffer ($clientRowBuffer) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->clientRowBuffer = $clientRowBuffer;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return int|NULL
+	 */
+	public function GetClientRowBuffer () {
+		return $this->clientRowBuffer;
+	}
+	
+	/**
+	 * 
+	 * @param  int|NULL $clientMaxRowsInCache 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetClientMaxRowsInCache ($clientMaxRowsInCache) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->clientMaxRowsInCache = $clientMaxRowsInCache;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return int|NULL
+	 */
+	public function GetClientMaxRowsInCache () {
+		return $this->clientMaxRowsInCache;
+	}
+
+	/**
+	 * 
+	 * @param  bool $clientChangeHistory 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetClientChangeHistory ($clientChangeHistory) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->clientChangeHistory = $clientChangeHistory;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return bool
+	 */
+	public function GetClientChangeHistory () {
+		return $this->clientChangeHistory;
+	}
+	
+	/**
+	 * 
+	 * @param  string|NULL $clientTitleTemplate
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetClientTitleTemplate ($clientTitleTemplate) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->clientTitleTemplate = $clientTitleTemplate;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return string|NULL
+	 */
+	public function GetClientTitleTemplate () {
+		return $this->clientTitleTemplate;
+	}
+	
+	/**
+	 * 
+	 * @param  int $dataRequestMethod 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetDataRequestMethod ($dataRequestMethod) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->dataRequestMethod = $dataRequestMethod;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return int
+	 */
+	public function GetDataRequestMethod () {
+		return $this->dataRequestMethod;
+	}
+	
+	/**
+	 * 
+	 * @param  string $urlData 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetUrlData ($urlData) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->urlData = $urlData;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return string|NULL
+	 */
+	public function GetUrlData () {
+		return $this->urlData;
+	}
+	
+	/**
+	 * 
 	 * @param  string $urlColumnsStates
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
 	 */
@@ -325,7 +354,7 @@ trait ConfigGettersSetters {
 	}
 	
 	/**
-	 * @inheritDocs
+	 * 
 	 * @return string
 	 */
 	public function GetUrlColumnsStates () {
@@ -339,9 +368,9 @@ trait ConfigGettersSetters {
 		}
 		return $this->urlColumnsStates;
 	}
-
+	
 	/**
-	 * @inheritDocs
+	 * 
 	 * @param  string $urlColumnsChanges
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
 	 */
@@ -351,7 +380,7 @@ trait ConfigGettersSetters {
 	}
 	
 	/**
-	 * @inheritDocs
+	 * 
 	 * @return string
 	 */
 	public function GetUrlColumnsChanges () {
@@ -365,9 +394,9 @@ trait ConfigGettersSetters {
 		}
 		return $this->urlColumnsChanges;
 	}
-
+	
 	/**
-	 * @inheritDocs
+	 * 
 	 * @param  \MvcCore\Ext\Controllers\DataGrids\AgGrids\IHandlerColumnsRead|callable $handlerColumnsRead 
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
 	 */
@@ -377,7 +406,7 @@ trait ConfigGettersSetters {
 	}
 	
 	/**
-	 * @inheritDocs
+	 * 
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrids\IHandlerColumnsRead|callable
 	 */
 	public function GetHandlerColumnsRead () {
@@ -385,7 +414,7 @@ trait ConfigGettersSetters {
 	}
 	
 	/**
-	 * @inheritDocs
+	 * 
 	 * @param  \MvcCore\Ext\Controllers\DataGrids\AgGrids\IHandlerColumnsWrite|callable $handlerColumnsRead 
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
 	 */
@@ -395,10 +424,41 @@ trait ConfigGettersSetters {
 	}
 
 	/**
-	 * @inheritDocs
+	 * 
 	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrids\IHandlerColumnsWrite|callable
 	 */
 	public function GetHandlerColumnsWrite () {
 		return $this->handlerColumnsWrite;
 	}
+	
+	/**
+	 * 
+	 * @param  array<string, string> $ajaxParamsNames 
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetAjaxParamsNames (array $ajaxParamsNames) {
+		/** @var \MvcCore\Ext\Controllers\DataGrids\AgGrid $this */
+		$this->ajaxParamsNames = $ajaxParamsNames;
+		return $this;
+	}
+
+	/**
+	 * 
+	 * @return array<string, string>
+	 */
+	public function GetAjaxParamsNames () {
+		return $this->ajaxParamsNames;
+	}
+	
+	/**
+	 * @inheritDocs
+	 * @param  \int[] $countScales
+	 * @return \MvcCore\Ext\Controllers\DataGrids\AgGrid
+	 */
+	public function SetCountScales (array $countScales) {
+		parent::SetCountScales($countScales);
+		$this->countScalesCustomized = TRUE;
+		return $this;
+	}
+	
 }
--- src/MvcCore/Ext/Controllers/DataGrids/AgGrid/ConfigProps.php
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrid/ConfigProps.php
@@ -21,9 +21,11 @@ use \MvcCore\Ext\Controllers\DataGrids\AgGrid\IConstants;
 trait ConfigProps {
 
 	/**
-	 * @var string
+	 * Datagrid unique id, used to cache parsed columns configuration
+	 * and to identificate grid element on current page.
+	 * @var string|NULL
 	 */
-	protected $jsClassFullName = 'MvcCore.Ext.Controllers.DataGrids.AgGrid';
+	protected $id = NULL;
 
 	/**
 	 * Configuration object for datagrid parts, style and controls rendering.
@@ -35,87 +37,121 @@ trait ConfigProps {
 	protected $configRendering = NULL;
 
 	/**
-	 * @var string|NULL
+	 * Localization properties configuration object.
+	 * @var \MvcCore\Ext\Controllers\DataGrids\AgGrids\Configs\Locales|NULL
 	 */
-	protected $id = NULL;
+	protected $configLocales = NULL;
+
+
+	/**
+	 * Custom JS full class name. Class must extend JS class:
+	 * `MvcCore.Ext.Controllers.DataGrids.AgGrid`.
+	 * @var string
+	 */
+	protected $jsClassFullName = 'MvcCore.Ext.Controllers.DataGrids.AgGrid';
 	
 	/**
+	 * `TRUE` to serialize into JSON rows data only enabled columns only,
+	 * `FALSE` to serialize all columns including disabled. `TRUE` by default.
 	 * @var bool
 	 */
 	protected $enabledColumnsOnly = TRUE;
-	
-	/**
-	 * @var int|NULL
-	 */
-	protected $clientPageMode = NULL;
-	
-	/**
-	 * @var int|NULL
-	 */
-	protected $clientRequestBlockSize = NULL;
-	
-	/**
-	 * @var int|NULL
-	 */
-	protected $clientRowBuffer = NULL;
 
 	/**
-	 * @var string|NULL
-	 */
-	protected $urlData = NULL;
-
-	/**
-	 * @var \DateInterval|NULL
-	 */
-	protected $timeZoneOffset = NULL;
-	
-	/**
-	 * @var int
-	 */
-	protected $dataRequestMethod = IConstants::AJAX_DATA_REQUEST_METHOD_GET;
-
-	/**
+	 * Grid row selection mode flags.
 	 * @var int
 	 */
 	protected $rowSelection = IConstants::ROW_SELECTION_NONE;
 
 	/**
-	 * 
+	 * User time zone offset.
+	 * @var \DateInterval|NULL
+	 */
+	protected $timeZoneOffset = NULL;
+
+	/**
+	 * `TRUE` to cache data in client browser.
 	 * @var bool
 	 */
 	protected $clientCache = TRUE;
 	
 	/**
+	 * Client page mode:
+	 * - `\MvcCore\Ext\Controllers\DataGrids\AgGrid::CLIENT_PAGE_MODE_MULTI`
+	 *   Client side rendered with only data for current page.
+	 * - `\MvcCore\Ext\Controllers\DataGrids\AgGrid::CLIENT_PAGE_MODE_SINGLE`
+	 *   Client side rendered with continuous data loading by user scrolling.
+	 * @var int|NULL
+	 */
+	protected $clientPageMode = NULL;
+	
+	/**
+	 * How many rows for each block in the store, i.e. how many 
+	 * rows returned from the server at a time. Default: 100.
+	 * @var int|NULL
+	 */
+	protected $clientRequestBlockSize = NULL;
+	
+	/**
+	 * AgGrid configuration property. The number of rows rendered 
+	 * outside the viewable area the grid renders. Having a buffer 
+	 * means the grid will have rows ready to show as the user 
+	 * slowly scrolls vertically. Default: 10.
+	 * @var int|NULL
+	 */
+	protected $clientRowBuffer = NULL;
+	
+	/**
+	 * Maximum number of rows in client browser cache.
 	 * @var int|NULL
 	 */
 	protected $clientMaxRowsInCache = NULL;
-
-	/**
-	 * 
-	 * @var \MvcCore\Ext\Controllers\DataGrids\AgGrids\Configs\Locales|NULL
-	 */
-	protected $configLocales = NULL;
 	
 	/**
-	 * 
+	 * `TRUE` to change client browser URL for each 
+	 * page change, sort change or filter change.
+	 * @var bool
+	 */
+	protected $clientChangeHistory = TRUE;
+	
+	/**
+	 * Client document title template.
+	 * @var string|NULL
+	 */
+	protected $clientTitleTemplate = FALSE;
+	
+	/**
+	 * Custom AJAX request method to load grid rows data.
+	 * @var int
+	 */
+	protected $dataRequestMethod = IConstants::AJAX_DATA_REQUEST_METHOD_GET;
+
+	/**
+	 * Custom URL to load grid rows data by AJAX.
+	 * @var string|NULL
+	 */
+	protected $urlData = NULL;
+	
+	/**
+	 * Custom URL to submit client columns enabled/disabled states.
 	 * @var string|NULL
 	 */
 	protected $urlColumnsStates = NULL;
 	
 	/**
-	 * 
+	 * Custom URL to send client columns order changes by AJAX.
 	 * @var string|NULL
 	 */
 	protected $urlColumnsChanges = NULL;
 
 	/**
-	 * 
+	 * Custom handler to read user column configurations from any place.
 	 * @var \MvcCore\Ext\Controllers\DataGrids\AgGrids\IHandlerColumnsRead|callable
 	 */
 	protected $handlerColumnsRead = NULL;
 
 	/**
-	 * 
+	 * Custom handler to write user column configurations into any place.
 	 * @var \MvcCore\Ext\Controllers\DataGrids\AgGrids\IHandlerColumnsWrite|callable
 	 */
 	protected $handlerColumnsWrite = NULL;

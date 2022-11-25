9109b73e4ead75c9660741b114b6ee1243d09b72
Tom Flídr <tomflidr@gmail.com>
2022-11-25 16:13:22 +0100

    Empty trait with `GetJsFormatter` function for `ReverseHelper`.

---
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrids/Views/ReverseHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * MvcCore
+ *
+ * This source file is subject to the BSD 3 License
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that are distributed with this source code.
+ *
+ * @copyright	Copyright (c) 2016 Tom Flidr (https://github.com/mvccore)
+ * @license		https://mvccore.github.io/docs/mvccore/5.0.0/LICENSE.md
+ */
+
+namespace MvcCore\Ext\Controllers\DataGrids\AgGrids\Views;
+
+trait ReverseHelper {
+
+	/**
+	 * Get valid javascript function as string to format cell value.
+	 * Example:
+	 * ```
+	 * function (params, propName, parserArgs, formatArgs) {
+	 *     return params.data[propName] ? "Yes" : "No";
+	 * }
+	 * ```
+	 * @return string
+	 */
+	public function GetJsFormatter (): string {
+		return 'function (params, propName, parserArgs, formatArgs) { return params.data[propName]; }';
+	}
+}

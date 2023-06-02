1fb694303b62475ea9f7a4651ccaba455295b833
Tom Flídr <tomflidr@gmail.com>
2023-06-02 16:00:37 +0200

    release v5.2.15

--- src/MvcCore/Ext/Controllers/DataGrids/AgGrid.php
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrid.php
@@ -34,7 +34,7 @@ implements	IConstantsAg {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.14';
+	const VERSION = '5.2.15';
 	
 	/**
 	 * Internal datagrid actions.

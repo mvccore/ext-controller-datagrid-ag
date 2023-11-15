01a3caa95d63c59fbbe6f4a57b9bb42c6f30d455
Tom Flídr <tomflidr@gmail.com>
2023-11-15 15:13:15 +0100

    Exposed agGrid.RowDataTransaction<T> type.

--- src/MvcCore/Ext/Controllers/DataGrids/AgGrids/Models/IGridRow.php
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrids/Models/IGridRow.php
@@ -16,13 +16,25 @@ namespace MvcCore\Ext\Controllers\DataGrids\AgGrids\Models;
 interface IGridRow extends \MvcCore\Ext\Controllers\DataGrids\Models\IGridRow {
 	
 	/**
-	 * 
+	 * Get metadata collection to set up database values 
+	 * inside current row class more faster in in `SetValues()` method
+	 * or serialize data more faster in method `$row->jsonSerialize()`.
+	 * @return array|NULL
+	 */
+	public static function GetActiveColumns ();
+	
+	/**
+	 * Set metadata collection to set up database values 
+	 * inside current row class more faster in in `SetValues()` method
+	 * or serialize data more faster in method `$row->jsonSerialize()`.
 	 * @param array $jsonProperties 
 	 */
 	public static function SetActiveColumns (array $activeColumns);
 
 	/**
-	 * 
+	 * Set up database values into current row class more faster 
+	 * then in `\MvcCore\Ext\Models\Db\Model::SetValues()` method.
+	 * Set up only active columns to serialize less data later.
 	 * @param  array $data       Raw data from database (row) or from form fields.
 	 * @param  int   $propsFlags All properties flags are available.
 	 * @throws \InvalidArgumentException
@@ -31,7 +43,7 @@ interface IGridRow extends \MvcCore\Ext\Controllers\DataGrids\Models\IGridRow {
 	public function SetValues ($data = [], $propsFlags = 0);
 
 	/**
-	 * 
+	 * Serialize row instance class into JSON format.
 	 * @param  int $propsFlags
 	 * @return array<string, mixed>
 	 */

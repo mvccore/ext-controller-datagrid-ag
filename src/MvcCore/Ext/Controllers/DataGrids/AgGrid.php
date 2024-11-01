5783ad026e6b5afd2c7ae90bdb44a7e06e9c6bc9
Tom Flídr <tomflidr@gmail.com>
2024-11-01 13:08:14 +0100

    release v5.2.35

--- src/MvcCore/Ext/Controllers/DataGrids/AgGrid.php
+++ src/MvcCore/Ext/Controllers/DataGrids/AgGrid.php
@@ -34,7 +34,7 @@ implements	\MvcCore\Ext\Controllers\DataGrids\IAgGrid {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.34';
+	const VERSION = '5.2.35';
 	
 	/**
 	 * Internal datagrid actions.
